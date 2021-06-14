58175dae00150945233b781cc5e9eea90bb3b7e6
piguanghua <piguanghua@163.com>
2021-06-15 03:10:38 +0800

    transformer、sink、source、partition、richfunction

---  readme.md
+++  readme.md
@@ -1,2 +1,5 @@
 项目:
 https://github.com/BigDataScholar/FlinkECUserBehaviorAnalysis/tree/master/src/main/java/com/hypers
+
+kafka-console-consumer.sh --bootstrap-server 10.103.17.101:9092,10.103.17.102:9092,10.103.17.103:9092 --topic test_pi --group flink
+kafka-consumer-groups.sh --bootstrap-server 10.103.17.101:9092,10.103.17.102:9092,10.103.17.103:9092 --reset-offsets --to-latest  --topic test_pi --group flink --execute
---
+++ src/main/java/com/study/flink/window/WindowApiDemo3.java
@@ -0,0 +1,67 @@
+package com.study.flink.window;
+
+import com.study.flink.model.SensorReading;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.util.Properties;
+
+public class WindowApiDemo3 {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+//        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        String topic = "test_pi";
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "10.103.17.101:9092,10.103.17.102:9092,10.103.17.103:9092,10.103.17.104:9092");
+        // 下面这些次要参数
+        properties.setProperty("group.id", "flink");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+
+        DataStream<String> dataStream = env.addSource(new FlinkKafkaConsumer011<String>(topic, new SimpleStringSchema(), properties));
+
+
+        dataStream.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String s) throws Exception {
+                String[] strs = s.split(",");
+                return new SensorReading(strs[0], Long.valueOf(strs[1]), Double.valueOf(strs[2]));
+            }
+        }).keyBy("name")
+                .timeWindow(Time.seconds(4L))
+            .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
+
+                @Override
+                public Integer createAccumulator() {
+                    return 0;
+                }
+
+                @Override
+                public Integer add(SensorReading sensorReading, Integer integer) {
+                    return integer + 1;
+                }
+
+                @Override
+                public Integer getResult(Integer integer) {
+                    return integer;
+                }
+
+                @Override
+                public Integer merge(Integer integer, Integer acc1) {
+                    return integer + acc1;
+                }
+            }).print();
+
+
+        env.execute();
+    }
+}
